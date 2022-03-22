ed143e860f2b4c37cd24020d8778f670af81946c
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-03-22 17:36:06 +0100

    Obtaining File in the Controller

--- controllers/storage.js
+++ controllers/storage.js
@@ -1,4 +1,5 @@
 const { storageModel } = require('../models')
+const PUBLIC_URL = process.env.PUBLIC_URL
 /**
  * Obtener lista de la base de datos!
  * @param {*} req
@@ -22,9 +23,12 @@ const getItem = (req, res) => {}
 const createItem = async (req, res) => {
 	const { body, file } = req
 	console.log(file)
-	// const data = await storageModel.create(body)
-	// await res.send({ data })
-	RTCRtpSender.send({ file })
+	const fileData = {
+		filename: file.filename,
+		url: `${PUBLIC_URL}/${file.filename}`,
+	}
+	const data = await storageModel.create(fileData)
+	res.send({ data })
 }
 /**
  * Actualizar un registro!
